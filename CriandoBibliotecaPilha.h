25fcacb925e749c54226385334252b25e5f0e6d7
VitorHSR18 <136871451+VitorHSR18@users.noreply.github.com>
2024-03-06 20:27:57 -0300

    Update CriandoBibliotecaPilha.h

--- CriandoBibliotecaPilha.h
+++ CriandoBibliotecaPilha.h
@@ -1,9 +1,6 @@
 #define MAXPILHA 10
 
-
-// não precisa das bibliotecas pq ja está incluida no programa principal e sempre tira os dados de cima , ou seja o ultimo que entra é o primeiro que sai (DADOS) LIFO ... " Last in , First Out " 
-
-struct TpPilha
+struct TpPilha1
 {
 	int TOPO;
 	char PILHA[MAXPILHA];
@@ -18,33 +15,33 @@ struct TpPilha
 	Exibe a pilha;
 */
 
-void Inicializar(TpPilha P);
+void Inicializar(TpPilha P)
 {
 	P.TOPO = -1; // Sempre precisamos inicializar uma pilha	
 }
 
-void Push(TpPilha &P,char Elemento);
+void Push(TpPilha &P,char Elemento)
 {
 	P.PILHA[++P.TOPO] = Elemento; // incrementa o topo devido a inicialização -1
 }
 
-char Pop(TpPilha &P);
+char Pop(TpPilha &P)
 {
-	return P.PILHA[P.TOPO--];	
+	return P.PILHA[P.TOPO--];
 }
 
-char ElementoTopo(TpPilha &P);
+char ElementoTopo(TpPilha &P)
 {
 	return P.PILHA[P.TOPO];
 }
 
-void Exibir(TpPilha P);
+void Exibir(TpPilha P)
 {
-	while(!Vazia(P.TOPO))
-    printf("\n%c",POP(P));
+		while (!Vazia(P.TOPO))
+			printf("\n%c",Pop(P));
 }
 
-int Vazia (int topo);
+int Vazia (int topo)
 {
 	if(topo == -1)
 		return 1;
@@ -52,10 +49,10 @@ int Vazia (int topo);
 		return 0;
 }
 
-int Cheia(int topo);
+int Cheia(int topo)
 {
 	if(topo == MAXPILHA-1)
-		return 1
+		return 1;
 	else
 		return -1;
 }
